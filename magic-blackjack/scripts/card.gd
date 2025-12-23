33e866ec3dfe7eb17ead725655b8ab4b7d66f9f7
Karim Addi <kaaddi@student.ethz.ch>
2025-12-23 01:03:14 +0100

    Implemented simple blackjack mechanism

--- magic-blackjack/scripts/card.gd
+++ magic-blackjack/scripts/card.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 @onready var label: Label = $Label
 
@@ -8,5 +8,24 @@ enum CardType { ACE, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, 
 @export var card_suit: Suit
 @export var card_value: CardType
 
+const card_width: int = 68
+const card_height: int = 122
+
 func _ready() -> void:
 	label.text = "suit: %s \n value %s" % [card_suit, card_value]
+
+func get_card_shape() -> Array:
+	return [card_width, card_height]
+
+func get_card_value() -> int:
+	match card_value:
+		CardType.ACE:
+			return 11
+		CardType.JACK:
+			return 10
+		CardType.QUEEN:
+			return 10
+		CardType.KING:
+			return 10
+	
+	return card_value
--- magic-blackjack/scripts/card_manager.gd
+++ magic-blackjack/scripts/card_manager.gd
@@ -2,7 +2,11 @@ extends Node
 
 @onready var player_cards: HBoxContainer = $player_cards
 
+const screen_width: int = 1280
+const screen_height: int = 720
+
 var rng = RandomNumberGenerator.new()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,9 +17,15 @@ func _ready() -> void:
 		instance.card_suit = rng.randi_range(0,3)
 		instance.card_value = rng.randi_range(0,13)
 		
-		instance.position.x = 100*i
+		#instance.position.x = 100*i
 		#instance.position.y = 560
+		
+		card_array.append(instance)
 		
 		player_cards.add_child(instance)
 		
 		print(instance.get_node("Label").text)
+		
+
+func _process(delta: float) -> void:
+	pass
