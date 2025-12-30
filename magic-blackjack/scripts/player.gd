96a2dcf462a7e8a27c3a4617c1128c90ed5198c0
Karim Addi <kaaddi@student.ethz.ch>
2025-12-30 16:01:55 +0100

    Added some money and health aspect

--- magic-blackjack/scripts/player.gd
+++ magic-blackjack/scripts/player.gd
@@ -15,7 +15,10 @@ func _on_stand_pressed() -> void:
 			
 		if dealer_score > 21 or dealer_score < card_manager.get_total_card_value():
 			win()
-			
+		
+		elif dealer_score == card_manager.get_total_card_value():
+			tie()
+		
 		else:
 			lose()
 
@@ -31,5 +34,10 @@ func bust() -> void:
 	
 func win() -> void:
 	print("win")
+	await get_tree().create_timer(1).timeout
+	get_tree().reload_current_scene()
+
+func tie() -> void:
+	print("tie")
 	await get_tree().create_timer(1).timeout
 	get_tree().reload_current_scene()
